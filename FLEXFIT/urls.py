ed49014808c7a73b2c226e788014cf12623f3d95
junniorhdp <junnior.gonzales.psw@gmail.com>
2026-06-21 18:47:41 -0500

    Fix re_path import

--- FLEXFIT/urls.py
+++ FLEXFIT/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import media_view
 
 
 urlpatterns = [
